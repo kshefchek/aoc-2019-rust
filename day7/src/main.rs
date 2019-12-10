c141926de7cd96419c69fdfec1159e67b28e8b36
kshefchek <kshefchek@gmail.com>
2019-12-09 23:21:04 -0800

    day 7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -5,20 +5,57 @@ use std::io::{BufRead, BufReader, Error};
 
 
 fn main() {
-    // https://adventofcode.com/2019/day/5
-    let mut intcode = get_intcode().unwrap();
-    println!("Intcode is {:?}", intcode);
+    // https://adventofcode.com/2019/day/7
+    let intcode = get_intcode().unwrap();
 
-    let mut input = 1;
-    let mut output = parse_intcode(&mut intcode, input);
-    println!("Output code for part one {:?}", output); // 13818007
+    let phases = vec![0,1,2,3,4];
+    let input = 0;
 
-    input = 5;
-    // Reset the intcode instructions
-    intcode = get_intcode().unwrap();
-    output = parse_intcode(&mut intcode, input);
-    println!("Output code for part two {:?}", output); // 3176266
+    let mut phase_setting = Vec::with_capacity(5);
+    let mut accum = Vec::new();
+    let mut level = 0;
 
+    get_signals(&intcode, &phases, &input, &mut phase_setting, &mut level, &mut accum);
+
+    let max: i32 = accum.iter().map(|x| x.1).max().unwrap();
+
+    println!("{:?}", max);
+}
+
+fn get_signals (
+    intcode: &Vec<i32>,
+    phases: &Vec<i32>,
+    input: &i32,
+    phase_setting: &mut Vec<i32>,
+    level: &mut u8,
+    accum: &mut Vec<(Vec<i32>, i32)>
+) {
+
+    let intcopy = intcode.clone();
+
+    for phase in phases {
+        phase_setting.push(*phase);
+
+        let mut inputs = vec![*phase, *input].into_iter();
+        let (new_intcode, new_input) = parse_intcode(&intcopy, &mut inputs);
+        let new_phases: Vec<i32> = phases
+            .iter()
+            .cloned()
+            .filter(|x| x != phase)
+            .collect();
+
+        if new_phases.len() == 0 {
+            let final_phases = phase_setting.to_vec();
+            accum.push((final_phases, new_input));
+        } else {
+            *level += 1;
+            get_signals(&new_intcode, &new_phases, &new_input, phase_setting, level, accum);
+        }
+    }
+    if *level > 0 {
+        *level -= 1;
+        phase_setting.truncate(*level as usize);
+    }
 }
 
 fn get_intcode () -> Result<Vec<i32>, Error> {
@@ -35,25 +72,28 @@ fn get_intcode () -> Result<Vec<i32>, Error> {
     Ok(intcode)
 }
 
-fn parse_intcode (intcode: &mut Vec<i32>, input: i32) -> Vec<i32> {
+fn parse_intcode <I>(intcode: &Vec<i32>, input: &mut I) -> (Vec<i32>, i32)
+    where I: Iterator<Item = i32>
+{
+    let mut intcopy = intcode.clone();
     let mut output: Vec<i32> = Vec::new();
     let mut frame: Vec<usize> = Vec::with_capacity(4);
     let mut buff_iter = 0..intcode.len();
 
     while let Some(i) = buff_iter.next() {
         frame.clear();
-        let opcode = get_opcode(intcode[i] as usize);
+        let opcode = get_opcode(intcopy[i] as usize);
         match opcode {
             1 | 2 | 7 | 8 => {
-                frame = intcode[i..i+4].into_iter().map(|val| *val as usize).collect();
+                frame = intcopy[i..i+4].into_iter().map(|val| *val as usize).collect();
                 buff_iter.nth(2);
             },
             3 | 4 => {
-                frame = intcode[i..i+2].into_iter().map(|val| *val as usize).collect();
+                frame = intcopy[i..i+2].into_iter().map(|val| *val as usize).collect();
                 buff_iter.nth(0);
             },
             5 | 6 => {
-                frame = intcode[i..i+3].into_iter().map(|val| *val as usize).collect();
+                frame = intcopy[i..i+3].into_iter().map(|val| *val as usize).collect();
             },
             99 => break,
             _ =>  {
@@ -61,12 +101,12 @@ fn parse_intcode (intcode: &mut Vec<i32>, input: i32) -> Vec<i32> {
                 break;
             },
         }
-        let instructions = process_parameter_mode(&frame, intcode);
+        let instructions = process_parameter_mode(&frame, &intcopy);
 
         match opcode {
-            1 => intcode[frame[3]] = instructions[0] + instructions[1],
-            2 => intcode[frame[3]] = instructions[0] * instructions[1],
-            3 => intcode[frame[1]] = input,
+            1 => intcopy[frame[3]] = instructions[0] + instructions[1],
+            2 => intcopy[frame[3]] = instructions[0] * instructions[1],
+            3 => intcopy[frame[1]] = input.next().unwrap(),
             4 => output.push(instructions[0]),
             5 => {
                 if instructions[0] != 0 {
@@ -86,23 +126,26 @@ fn parse_intcode (intcode: &mut Vec<i32>, input: i32) -> Vec<i32> {
             },
             7 => {
                 if instructions[0] < instructions[1] {
-                    intcode[frame[3]] = 1;
+                    intcopy[frame[3]] = 1;
                 } else {
-                    intcode[frame[3]] = 0;
+                    intcopy[frame[3]] = 0;
                 }
             },
             8 => {
                 if instructions[0] == instructions[1] {
-                    intcode[frame[3]] = 1;
+                    intcopy[frame[3]] = 1;
                 } else {
-                    intcode[frame[3]] = 0;
+                    intcopy[frame[3]] = 0;
                 }
             },
             99 => break,
             _ => panic!("Cannot parse opcode {:?}", opcode),
         }
+        if output.len() > 0 {
+            break;
+        }
     }
-    output
+    (intcopy, output[0])
 }
 
 fn process_parameter_mode(frame: &Vec<usize>, memory: &Vec<i32>) -> Vec<i32> {
